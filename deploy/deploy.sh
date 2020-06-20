20ca7877699091fb97d399569fe4d797b17ae44f
Josh Long <josh@joshlong.com>
2020-06-19 20:32:22 -0700

    fix the deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,7 +6,7 @@ export APP_NAME=editor-api
 export CF_APP_NAME=ttd-${APP_NAME}
 export CF_DB_SVC_NAME=ttd-db
 
-cf push --no-start -p target/editor-api.jar ${CF_APP_NAME}
+cf push --no-start -p target/bookmark-api.jar ${CF_APP_NAME}
 cf bs $CF_APP_NAME $CF_DB_SVC_NAME
 cf set-env ${CF_APP_NAME} JBP_CONFIG_OPEN_JDK_JRE '{ jre: { version: 11.+}}'
 cf set-env ${CF_APP_NAME} PINBOARD_TOKEN $PINBOARD_TOKEN
