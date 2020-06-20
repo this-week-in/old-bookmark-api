c5d5f6c601ea41d29221d599b73f9535a87a28a5
Josh Long <josh@joshlong.com>
2020-06-19 21:04:45 -0700

    deploy fix for the DB

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,12 +5,13 @@ source "$(cd $(dirname $0) && pwd)/env.sh"
 export APP_NAME=bookmark-api
 export TWI_PREFIX=${TWI_PREFIX:-twi}
 export CF_APP_NAME=${TWI_PREFIX}-${APP_NAME}
-export CF_DB_SVC_NAME=${TWI_PREFIX}-db
+export DB_SVC_NAME=${TWI_PREFIX}-db
 
 cf push --no-start -p target/bookmark-api.jar ${CF_APP_NAME}
-cf bs $CF_APP_NAME $CF_DB_SVC_NAME
+cf bs $CF_APP_NAME $DB_SVC_NAME
 cf set-env ${CF_APP_NAME} JBP_CONFIG_OPEN_JDK_JRE '{ jre: { version: 11.+}}'
 cf set-env ${CF_APP_NAME} PINBOARD_TOKEN $PINBOARD_TOKEN
 cf set-env ${CF_APP_NAME} JWT_KEY ${JWT_KEY}
+cf set-env ${CF_APP_NAME} DB_SVC_NAME ${DB_SVC_NAME}
 cf set-env ${CF_APP_NAME} SPRING_PROFILES_ACTIVE cloud
 cf start ${CF_APP_NAME}
