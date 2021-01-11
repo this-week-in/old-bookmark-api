9ed5de0fa343c65f46d58e83d0f577b8bcca903b
Josh Long <josh@joshlong.com>
2021-01-11 02:24:12 -0800

    workflows

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash 
+PROJECT_ID=${GCLOUD_PROJECT}
+TAG_NAME=${1:-$(date +%s)}
+IMAGE_TAG="production${GITHUB_SHA:-}"
+GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
+
 ROOT_DIR=$(cd $(dirname $0) && pwd)
+APP_NAME=bookmark-api
+
 
 mvn -f ${ROOT_DIR}/../../pom.xml -e -Dspring.profiles.active=production  \
   clean \
@@ -7,3 +14,9 @@ mvn -f ${ROOT_DIR}/../../pom.xml -e -Dspring.profiles.active=production  \
   verify \
   deploy \
   spring-boot:build-image
+
+image_id=$(docker images -q $APP_NAME)
+docker tag "${image_id}" ${GCR_IMAGE_NAME}:latest
+docker tag "${image_id}" ${GCR_IMAGE_NAME}:${IMAGE_TAG}
+docker push ${GCR_IMAGE_NAME}:latest
+docker push ${GCR_IMAGE_NAME}:${IMAGE_TAG}
